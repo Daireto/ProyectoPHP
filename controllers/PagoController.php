de32ce542c9bd2b2424aca2375e3fc5a5bc0f299
Valeea03 <142603715+Valeea03@users.noreply.github.com>
2023-11-07 18:56:13 -0500

    Update PagoController.php

--- controllers/PagoController.php
+++ controllers/PagoController.php
@@ -1,1 +1,14 @@
+<?php
+require_once 'config/db.php';
+
+class Pago
+{
+    private $db;
+
+    private $codigo;
+    private $monto;
+    private $medio;
+    private $codigo_est;
+    private $fecha_creacion;
+    private $fecha_actualizacion;
 
